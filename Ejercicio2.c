a2a2e31872f2be351c46a0362094a90b80908c23
Amad3uXs <davidmartveg@gmail.com>
2025-10-17 13:44:26 -0600

    Create Ejercicio2.c

---
+++ Ejercicio2.c
@@ -0,0 +1,24 @@
+/*David Alejandro Martínez Vega L´Englisse
+clase 2 ejercicio 2*/
+
+#include <stdio.h>
+#include <math.h>
+
+int main() 
+{
+    int num1, num2, res;
+
+    printf("Escribe el primer numero: \n");
+    scanf("%d", &num1);
+
+    printf("Escribe el segundo numero: \n");
+    scanf("%d", &num2);
+
+    res = num1 % num2;
+
+    if (res == 0) {
+        printf("El numero es divisible \n");
+    } else {
+        printf("El numero no es divisible \n");
+    }
+}
